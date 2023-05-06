469c0ae07d62801eb02c6488ba5501bffa7807db
Juliana Ricardo <szric.ju@gmail.com>
2023-05-05 21:48:16 -0300

    feat: load data into the table with fetch

--- file.js
+++ file.js
@@ -6,3 +6,35 @@ mediaButton.onclick = function(){
   mainListDiv.classList.toggle("show_list");
   mediaButton.classList.toggle("active");
 };
+
+function getData() {
+  var saida = document.getElementById("departure").value;
+  var pontoFinal = document.getElementById("endpoint").value;
+  var url = "http://localhost:8080/lines/" + saida + "/" + pontoFinal;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      var tableBody = document.getElementById("table-body");
+      tableBody.innerHTML = "";
+
+      var tableHead = document.getElementById("table-head");
+      tableHead.innerHTML = "";
+
+      tableHead.insertAdjacentHTML('afterbegin', `
+      <tr>
+        <th>${saida} - ${pontoFinal}</th>
+      </tr>
+      `);
+
+      data.forEach(bus_line => {
+        tableBody.insertAdjacentHTML("beforeend", `
+        <tr>
+          <td>${bus_line.time}</td>
+          <td>${bus_line.obs}</td>
+        </tr>
+        `);
+      });
+      document.getElementById('tabela').classList.remove('hidden');
+      })
+}
